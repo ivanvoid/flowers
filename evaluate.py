56b332a14565a7dde743c2805022d7dd39a79590
Ivan Kakhaev <59028817+ivanvoid@users.noreply.github.com>
2022-06-02 14:08:00 +0800

    added F1 and total score

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,4 @@
+from sklearn.metrics import f1_score
 import pandas as pd
 
 true_file = 'original_training_set_labels.csv'
@@ -5,6 +6,23 @@ pred_file = 'submission.csv'
 
 true_data = pd.read_csv(true_file)
 pred_data = pd.read_csv(pred_file)
+
+true_y = []
+pred_y = []
+
+for i, row in true_data.iterrows():
+    filename = row[0]
+    class_id = row[1]
+
+    p_row = pred_data.loc[pred_data['filename'] == filename]
+    p_class = p_row['category'].values[0]
+    
+    true_y += [class_id]
+    pred_y += [p_class]
+
+f1_scores = f1_score(true_y, pred_y, average=None)
+m_f1 = f1_scores.mean()
+print('MakroF1:',m_f1)
 
 correct = 0
 total = 0
@@ -22,3 +40,5 @@ for i, row in true_data.iterrows():
     
 acc = correct / total
 print('Accuracy:',acc)
+print('-'*8)
+print('Total score:', 0.5*m_f1 + 0.5*acc)
